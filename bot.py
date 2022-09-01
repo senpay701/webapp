08832eb4519d518e14629a82e34fc228e7ce14b4
Senpai <103895149+senpay701@users.noreply.github.com>
2022-09-01 15:20:31 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 
-API_TOKEN = 'ТОКЕН'
+# Made by @Your_senpay
+
+API_TOKEN = 'YOUR_TOKEN'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    await message.answer("test", 
-reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text="test", 
+    await message.answer(f"Hello, {message.from_user.full_name}", 
+reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text="WebApp", 
 web_app=WebAppInfo(url="https://senpay701.github.io/webapp/"))))
 
 if __name__ == '__main__':
