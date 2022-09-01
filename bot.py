b4d0ca1a0cd8c20c0bda82144a9cb5c1c2bc1bef
Senpai <103895149+senpay701@users.noreply.github.com>
2022-09-01 15:32:37 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram.types.web_app_info import WebAppInfo
 # Made by @Your_senpay
 
 API_TOKEN = 'YOUR_TOKEN'
+URL = 'YOUR_LINK'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -13,7 +14,7 @@ dp = Dispatcher(bot)
 async def cmd_start(message: types.Message):
     await message.answer(f"Hello, {message.from_user.full_name}", 
 reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text="WebApp", 
-web_app=WebAppInfo(url="https://senpay701.github.io/webapp/"))))
+web_app=WebAppInfo(url=URL))))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
